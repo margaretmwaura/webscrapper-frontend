dcf293d8b71095aa1531dd842777c2ef2499f09a
margaretmwaura <mwauramargaret1@gmail.com>
2022-11-16 21:37:36 +0300

    tailwind and vuejs 3 setup ; the responsive navbar

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import NavBar from './components/NavBar.vue'
+</script>
+
+<template>
+  <NavBar/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+let open = ref(false)
+
+// functions that mutate state and trigger updates
+function toggle () {
+  open.value = !open.value
+  }
+
+</script>
+
+<template>
+
+<div class="font-sans antialiased">
+  <!-- We have removed flex-wrap -->
+  <nav class="flex items-center justify-between flex-wrap md:flex-nowrap bg-teal p-6">
+    <div class="flex items-center flex-no-shrink text-black mr-6">
+      <img src="src/assets/book-stack.png" class="w-4 h-4">
+      <span class="font-semibold text-xl tracking-tight">Bonjour!</span>
+    </div>
+    <div class="block md:hidden">
+      <button v-on:click="toggle" class="flex px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-green hover:border-green">
+        <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+      </button>
+    </div>
+    <!-- class remove is flex-grow -->
+    <div :class="open ? 'block': 'hidden'" class="w-full sm:w-42 flex md:flex justify-center sm:flex sm:items-center">
+      <!-- text-sm sm:flex-grow -->
+      <div class="text-sm flex-grow md:flex-none">
+        <a href="#responsive-header" class="no-underline block mt-4 sm:inline-block sm:mt-0 text-teal-lighter hover:text-green mr-4">
+          Docs
+        </a>
+        <a href="#responsive-header" class="no-underline block mt-4 sm:inline-block sm:mt-0 text-teal-lighter hover:text-green mr-4">
+          Examples
+        </a>
+        <a href="#responsive-header" class="no-underline block mt-4 sm:inline-block sm:mt-0 text-teal-lighter hover:text-green">
+          Blog
+        </a>
+      </div>
+    </div>
+    <div :class="open ? 'block': 'hidden'" class="md:flex  md:justify-end">
+      <!-- The block makes it to appear one after another -->
+        <a href="#" class="no-underline block text-sm px-4 py-2 leading-none border rounded text-green border-green hover:border-transparent hover:text-teal hover:bg-green mt-4 sm:mt-0">Download</a>
+        <a href="#" class="no-underline block text-sm px-4 py-2 leading-none border rounded text-green border-green hover:border-transparent hover:text-teal hover:bg-green mt-4 sm:mt-0">Download</a>
+    </div>
+   
+  </nav>
+</div>
+</template>
+
+<style scoped>
+
+</style>
