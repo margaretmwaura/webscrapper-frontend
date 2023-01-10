f73b93410075c3301b3e9116707df943fa0cfbe8
margaretmwaura <mwauramargaret1@gmail.com>
2023-01-10 06:48:39 +0300

    finalizing on the navbar; setting up the landing page

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,13 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import NavBar from './components/NavBar.vue'
+import Landing from './components/Landing.vue'
 </script>
 
 <template>
   <NavBar/>
+  <Landing/>
+  
 </template>
 
 <style scoped>
