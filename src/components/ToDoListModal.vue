ce4c57e2374d4c1df45d04e7d4377041b1cb0ad1
margaretmwaura <mwauramargaret1@gmail.com>
2023-08-08 04:39:57 +0300

    adding underlines in the text area, code cleanup

--- src/components/ToDoListModal.vue
+++ src/components/ToDoListModal.vue
@@ -103,25 +103,16 @@ export default{
       </div>
       <div className="flex flex-col w-full py-4">
         <p>“Each day I will accomplish one thing on my to do list.”― Lailah Gifty Akita 😊</p>
-        <div class="mt-10">
-          <!-- TODO: The styling will be different based on whether it is done or not -->
-          <!-- TODO: When you hover over a list you can mark it as done -->
-          <!-- <ul>
-            <li v-for="todo in todoListItems" :key="todo" >
-              {{todo}}
-            </li>
-          </ul> -->
+        <div class="mt-6">
           <textarea v-on:input="handleInput($event)" 
           rows="5" 
-          class="bg-transparent outline-none w-full"
+          class="notes bg-transparent outline-none w-full"
           placeholder="Create your todo list" ref="textarea" v-model="toDoList"></textarea>
         </div>
         <div class="flex justify-center">
-          <button class="mt-12 bg-indigo-700 text-white rounded-full py-2 disabled:opacity-25 w-32 " 
+          <button class="mt-8 bg-indigo-700 text-white rounded-full py-2 disabled:opacity-25 w-32 " 
           @click="saveToDoList()" :disabled="savingTodoList">Save To Do's</button>
-          <!-- TODO: Set the method to be one for notes and todo lists -->
         </div>
-         <!-- <p class="font-light text-sm mt-4 text-center">Wanted to add a new Note? <span class="font-medium">Switch to Notes</span></p> -->
       </div>
     </div>
 </div>
@@ -131,6 +122,16 @@ export default{
 
 a{
   list-style-type: circle;
+}
+
+.notes {
+    background-attachment: local;
+    background-image:
+        linear-gradient(to right, white 10px, transparent 10px),
+        linear-gradient(to left, white 10px, transparent 10px),
+        repeating-linear-gradient(white, white 30px, #ccc 30px, #ccc 31px, white 31px);
+    line-height: 31px;
+    padding: 8px 10px;
 }
 </style>
 
